5a43058c1634a42eb4baed7123f2a93af7bea380
josemak25 <amakirij@gmail.com>
2020-04-16 21:04:22 +0100

    add submit functionality for tailor script

--- js/tailors.js
+++ js/tailors.js
@@ -23,7 +23,34 @@ $(document).ready(async () => {
         return payload;
       });
 
-      $("#example").DataTable({ data });
+      // $("#example").DataTable({ data });
     },
+  });
+
+  /*Dropdown Menu*/
+  $(".dropdown").click(function () {
+    $(this).attr("tabindex", 1).focus();
+    $(this).toggleClass("active");
+    $(this).find(".dropdown-menu").slideToggle(300);
+  });
+
+  $(".dropdown").focusout(function () {
+    $(this).removeClass("active");
+    $(this).find(".dropdown-menu").slideUp(300);
+  });
+
+  $(".dropdown .dropdown-menu li").click(function () {
+    $(this).parents(".dropdown").find("span").text($(this).text());
+    $(this)
+      .parents(".dropdown")
+      .find("input")
+      .attr("value", $(this).attr("id"));
+  });
+
+  /*End Dropdown Menu*/
+
+  $(".dropdown-submit").click(function () {
+    const stateValue = $("#selected-state").val();
+    const lgaValue = $("#selected-lga").val();
   });
 });
