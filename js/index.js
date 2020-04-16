789d2b5c7608d74175a1a0db691f8e9fe929455c
josemak25 <amakirij@gmail.com>
2020-04-16 11:19:07 +0100

    add scroll to specific view

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@ const closeMobileMethod = function () {
   Array.from(menuListItem).forEach((item) => item.classList.remove("active"));
   this.classList.toggle("active");
   setTimeout(() => pageContainer.classList.remove("menu-show"), 300);
+  const contentID = this.children[0].attributes[0].value;
+
+  $("html,body").animate({ scrollTop: $(contentID).offset().top }, "slow");
 };
 
 if (pageContainer.clientWidth <= 1024) {
